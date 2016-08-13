f816e9756189dceb15a11981c3117675f5c9b114
great_king <297295825@qq.com>
2016-08-13 17:24:52 +0800

    修改

--- bluetooth/bluetooth/ViewController.m
+++ bluetooth/bluetooth/ViewController.m
@@ -10,15 +10,15 @@
 #import <CoreBluetooth/CoreBluetooth.h>
 @interface ViewController ()<CBCentralManagerDelegate,CBPeripheralDelegate>
 @property (nonatomic,strong) CBCentralManager *manager;
+@property (nonatomic,strong) CBPeripheral *Peripheral;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.manager  = [[CBCentralManager alloc] initWithDelegate:self queue:nil];  ;
-    
-    
+    self.manager  = [[CBCentralManager alloc] initWithDelegate:self queue:nil];
+
 }
 #pragma mark - CBCentralManagerDelegate 
 //检查蓝牙设备是否准备完毕
@@ -26,25 +26,29 @@
     if (central.state == 5) {
         NSLog(@"蓝牙设备已经准备完毕");
         //扫描蓝牙设备
-        [self.manager scanForPeripheralsWithServices:nil options:@{ CBCentralManagerScanOptionAllowDuplicatesKey : @YES }];
+        NSDictionary * dic = [NSDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithBool:false],CBCentralManagerScanOptionAllowDuplicatesKey,nil];
+        [self.manager scanForPeripheralsWithServices:nil options:dic];
     }
 }
 
 - (void)centralManager:(CBCentralManager *)central didDiscoverPeripheral:(CBPeripheral *)peripheral advertisementData:(NSDictionary *)advertisementData RSSI:(NSNumber *)RSSI {
     NSLog(@"%@",peripheral);
     NSLog(@"%@",RSSI);
-    if ([peripheral.name isEqualToString:@"My Mambo"]) {
+    
+    if ([peripheral.name isEqualToString:@"MI Band 2"]) {
         [self.manager stopScan];
-        [self.manager connectPeripheral:peripheral options:nil];
+        self.Peripheral = peripheral;//赋值给外围设备属性、保持强引用
+        [self.manager connectPeripheral:self.Peripheral options:nil];
+//        [self.manager connectPeripheral:peripheral options:[NSDictionary dictionaryWithObject:[NSNumber numberWithBool:YES]forKey:CBConnectPeripheralOptionNotifyOnDisconnectionKey]];
     }
 }
 
 // 连接到外设后
 - (void)centralManager:(CBCentralManager *)central didConnectPeripheral:(CBPeripheral *)peripheral{
     NSLog(@"已经连接到:%@", peripheral.description);
-//    peripheral.delegate = self;
-//    [central stopScan];
-//    [peripheral discoverServices:nil];
+    self.Peripheral = peripheral;
+    self.Peripheral.delegate = self;
+    [self.Peripheral discoverServices:nil];
 }
 
 // 连接失败后
@@ -57,4 +61,14 @@
 }
 
 
+#pragma mark - CBPeripheralDelegate
+- (void)peripheral:(CBPeripheral *)peripheral didDiscoverServices:(NSError *)error {
+    
+    NSLog(@"didDiscoverServices");
+
+}
+
+- (void)peripheral:(CBPeripheral *)peripheral didDiscoverIncludedServicesForService:(CBService *)service error:(nullable NSError *)error {
+    NSLog(@"didDiscoverIncluded");
+}
 @end
